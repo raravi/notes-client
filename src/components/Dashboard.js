7ac27fe6973ede86ccc25f66ffbea0b767b4f0ba
Amith Raravi <amith.raravi@gmail.com>
2020-03-04 02:55:00 +0100

    Added Login/Register/Forgot Password functionality.

---
+++ src/components/Dashboard.js
@@ -0,0 +1,99 @@
+import React from "react";
+import { onKeyDownInEditor, onClickInEditor } from '../editor/Editor';
+
+export const Dashboard = () => {
+  return (
+    <div className="container">
+      <div className="sidebar"></div>
+      <div className="mainbar">
+        <header className="header">
+          <div className="header__title">Notes</div>
+        </header>
+        <div contentEditable="true" className="note" onKeyDown={onKeyDownInEditor} onMouseUp={onClickInEditor}>
+          <div className="note__line">
+            <p className="note__header1">
+              <span className="note__text">#&nbsp;Header&nbsp;One</span>
+            </p>
+          </div>
+          <div className="note__line">
+            <p className="note__paragraph">
+              <span className="note__text"><br /></span>
+            </p>
+          </div>
+          <div className="note__line">
+            <p className="note__paragraph">
+              <span className="note__text"><br /></span>
+            </p>
+          </div>
+          <div className="note__line">
+            <p className="note__paragraph">
+              <span className="note__text">Sim</span>
+            </p>
+          </div>
+          <div className="note__line">
+            <p className="note__header1">
+              <span className="note__text">#&nbsp;Head</span>
+              <span className="note__bold">*er&nbsp;O*</span>
+            </p>
+          </div>
+          <div className="note__line">
+            <p className="note__paragraph">
+              <span className="note__text">Sim</span>
+              <span className="note__bold">*ple&nbsp;l*</span>
+              <span className="note__text">ine</span>
+            </p>
+          </div>
+          <div className="note__line">
+            <p className="note__blockquote">
+              <span className="note__text">&gt; You can have properly indented paragraphs within list items. Notice the blank line above, </span><span className="note__link">[I'm an inline-style link](www.google.com)</span><span className="note__text"> and the leading spaces (at least one, but we'll use three here to also align the raw Markdown).</span>
+            </p>
+          </div>
+        </div>
+        <footer className="footer">
+          <div className="footer__text">
+            <p>&#169; 2020 Amith Raravi - source code on <a href="https://github.com/raravi/notes-client">Github</a></p>
+          </div>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+/*
+<div className="note__line">
+  <span className="note__header1">
+    <span className="note__text"># Head</span>
+    <span className="note__bold">*er O*</span>
+    <span className="note__text">ne&nbsp;</span>
+  </span>
+</div>
+<div className="note__line">
+  <span className="note__paragraph">
+    <span className="note__text">Sim</span>
+    <span className="note__bold">*ple l*</span>
+    <span className="note__text">ine&nbsp;</span>
+  </span>
+</div>
+<div className="note__line">
+  <span className="note__paragraph">
+    <span className="note__text">You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we'll use three here to also align the raw Markdown).&nbsp;</span>
+  </span>
+</div>
+*/
+
+/*
+# How to write a blog post!
+1. *Come up with an interesting topic*: This can be anything. But it should be something that you're good at. Only then will you be able to write about it in detail.
+2. *Research on it*: Do your bit before you start to write. Make a habit of researching about the topic, and check all available literature on it. So that you get an idea of how things stand now.
+3. *A broad outline*: Break down the topic into sub-topics so that you can structure your post in a meaningful way.
+4. *Write write write*: Just start typing, that's all there is to it. Just keep typing out the thoughts that come randomly into your head. Get everything out.
+5. *Edit it*: Once you have typed out everything in your head, then it's time to start structuring it into the outline you had come up with earlier. It's okay if the outline changes, as long as it makes sense!
+## Why should I write a blog post?
+Writing is a form of expression. We humans have an innate need to express ourselves and be heard. We want to see and wish to be seen. And writing is one of the mediums for doing so. It also helps us become better at thinking objectively and form opinions based on research in a coherent manner.
+## How to build an audience
+In order to build a loyal following of readers willing to go through what you put up, you need to write good content consistently. And make sure to keep an eye on quality. Follow these guidelines:
+* Write every day
+* Never compromise on quality.
+* Always think about the people who will read your content
+* Are you saying something different than others?
+*/
---
+++ src/components/Login.js
@@ -0,0 +1,194 @@
+import React, { useState } from 'react';
+import { LoginSection } from './LoginSection';
+import { RegisterSection } from './RegisterSection';
+import { ForgotPasswordSection } from './ForgotPasswordSection';
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
+
+export const Login = (props) => {
+  let [ forgotPassword, setForgotPassword ] = useState(false);
+  let [ newUser, setNewUser ] = useState(false);
+  let [ loginEmailError, setLoginEmailError ] = useState('');
+  let [ loginPasswordError, setLoginPasswordError ] = useState('');
+  let [ registerUsernameError, setRegisterUsernameError ] = useState('');
+  let [ registerEmailError, setRegisterEmailError ] = useState('');
+  let [ registerPasswordError, setRegisterPasswordError ] = useState('');
+  let [ registerPassword2Error, setRegisterPassword2Error ] = useState('');
+  let [ registerSuccess, setRegisterSuccess ] = useState('');
+  let [ forgotPasswordEmailError, setForgotPasswordEmailError ] = useState('');
+  let [ forgotPasswordEmailSuccess, setForgotPasswordEmailSuccess ] = useState('');
+
+  /**
+   * These functions toggle/set state variables.
+   */
+  function toggleNewUser() {
+    setNewUser(!newUser);
+  }
+  function onClickForgotPassword() {
+    setForgotPassword(true);
+  }
+
+  /**
+   * Try to login the user, send the details to the server
+   * and display the server response to the user.
+   */
+  function login() {
+    var email = document.getElementsByClassName("login__email")[0];
+    var password = document.getElementsByClassName("login__password")[0];
+
+    setLoginEmailError('');
+    setLoginPasswordError('');
+
+    /**
+     * POST the user request to the API endpoint '/login'.
+     */
+    axios.post('http://localhost:8000/api/users/login', {
+      email: email.value,
+      password: password.value
+    })
+    .then(function (response) {
+      sessionStorage.setItem("token", response.data.token);
+      let tokenDecoded = jwtDecode(response.data.token);
+      if(tokenDecoded) {
+        props.setUserLoggedIn({id: tokenDecoded.id, name: tokenDecoded.name});
+        // TODO: Do Login stuff
+      }
+    })
+    .catch(function (error) {
+      if (error.response) {
+        if (error.response.data) {
+          if (error.response.data.email) {
+            setLoginEmailError(error.response.data.email);
+          }
+          if (error.response.data.password) {
+            setLoginPasswordError(error.response.data.password);
+          }
+        }
+      } else {
+        console.log(error);
+      }
+    });
+  }
+
+  /**
+   * Try to register the new user, send the details to the server
+   * and display the server response to the user.
+   */
+  function register () {
+    var username = document.getElementsByClassName("register__username")[0];
+    var email = document.getElementsByClassName("register__email")[0];
+    var password = document.getElementsByClassName("register__password")[0];
+    var password2 = document.getElementsByClassName("register__password2")[0];
+
+    setRegisterUsernameError('');
+    setRegisterEmailError('');
+    setRegisterPasswordError('');
+    setRegisterPassword2Error('');
+    setRegisterSuccess('');
+
+    /**
+     * POST the user request to the API endpoint '/register'.
+     */
+    axios.post('http://localhost:8000/api/users/register', {
+      name: username.value,
+      email: email.value,
+      password: password.value,
+      password2: password2.value
+    })
+    .then(function (response) {
+      setRegisterSuccess(response.data.createduser);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        if (error.response.data) {
+          if (error.response.data.name) {
+            setRegisterUsernameError(error.response.data.name);
+          }
+          if (error.response.data.email) {
+            setRegisterEmailError(error.response.data.email);
+          }
+          if (error.response.data.password) {
+            setRegisterPasswordError(error.response.data.password);
+          }
+          if (error.response.data.password2) {
+            setRegisterPassword2Error(error.response.data.password2);
+          }
+        }
+      } else {
+        console.log(error);
+      }
+    });
+  }
+
+  /**
+   * Try to initiate the reset of the user's password, send
+   * the details to the server and display the server response
+   * to the user.
+   */
+  function sendForPasswordReset() {
+    var email = document.getElementsByClassName("forgot-password__email")[0];
+
+    setForgotPasswordEmailError('');
+    setForgotPasswordEmailSuccess('');
+
+    /**
+     * POST the user request to the API endpoint '/forgotpassword'.
+     */
+    axios.post('http://localhost:8000/api/users/forgotpassword', {
+      email: email.value
+    })
+    .then(function (response) {
+      if (response.data.emailsent)
+        setForgotPasswordEmailSuccess(response.data.emailsent);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        if (error.response.data) {
+          if (error.response.data.email) {
+            setForgotPasswordEmailError(error.response.data.email);
+          }
+        }
+      } else {
+        console.log(error);
+      }
+    });
+  }
+
+  return (
+    <>
+      <header className="login-header">
+        <h1 className="login-header__title">Notes</h1>
+      </header>
+      { !forgotPassword && !newUser &&
+        <LoginSection
+          toggleNewUser={toggleNewUser}
+          loginEmailError={loginEmailError}
+          loginPasswordError={loginPasswordError}
+          login={login}
+          onClickForgotPassword={onClickForgotPassword}
+        />
+      }
+      { !forgotPassword && newUser &&
+        <RegisterSection
+          toggleNewUser={toggleNewUser}
+          registerUsernameError={registerUsernameError}
+          registerEmailError={registerEmailError}
+          registerPasswordError={registerPasswordError}
+          registerPassword2Error={registerPassword2Error}
+          registerSuccess={registerSuccess}
+          register={register}
+        />
+      }
+      { forgotPassword &&
+        <ForgotPasswordSection
+          forgotPasswordEmailError={forgotPasswordEmailError}
+          forgotPasswordEmailSuccess={forgotPasswordEmailSuccess}
+          sendForPasswordReset={sendForPasswordReset}
+        />
+      }
+      <footer className="login-footer">
+        <p>&#169; 2020 Amith Raravi - source code on <a href="https://github.com/raravi/notes-client">Github</a></p>
+      </footer>
+    </>
+  )
+}
