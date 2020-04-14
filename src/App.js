6b1394746c216898a7cd5843c1fc51cbc0729b39
Amith Raravi <amith.raravi@gmail.com>
2020-04-14 18:22:32 +0200

    Added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import { Dashboard } from './components/Dashboard';
 import { Login } from './components/Login';
 import './App.css';
 
 /**
  * The main App component.
- * Routes to Login / Dashboard React Components based upon the LOGIN state. 
+ * Routes to Login / Dashboard React Components based upon the LOGIN state.
  */
 function App() {
   let [ userLoggedIn, setUserLoggedIn ] = useState(null);
   let [ notes, setNotes ] = useState([]);
 
   return (
-    <>
-      {userLoggedIn &&
-        <Dashboard  setUserLoggedIn={setUserLoggedIn}
-                    notes={notes}
-                    setNotes={setNotes}
-                    userId={userLoggedIn.id} />}
-      {!userLoggedIn &&
-        <Login  setUserLoggedIn={setUserLoggedIn}
-                setNotes={setNotes} />}
-    </>
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          { userLoggedIn
+            ? <Dashboard  setUserLoggedIn={setUserLoggedIn}
+                          notes={notes}
+                          setNotes={setNotes}
+                          user={userLoggedIn} />
+            : <Redirect to="/" />
+          }
+        </Route>
+        <Route path="/">
+          { userLoggedIn
+            ? <Redirect to="/dashboard" />
+            : <Login  setUserLoggedIn={setUserLoggedIn}
+                      setNotes={setNotes} />
+          }
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
